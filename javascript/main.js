910eefdd9b557238c80ee6f5663e82b19b36d865
DeFariasConsulting <defariasconsulting@gmail.com>
2019-03-18 10:01:04 -0400

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,37 @@
+//5 Questions true or false & yes or no
+//Wu Tang name based on the answer
+//Write a loop that itterates through 5 questions
+//loop checks how many true or false there are
+//depending on the numbers of trues loops will output a name
+//radio buttons
+t = true;
+f = false;
+
+//want to run function on click of submit button
+document.querySelector("button").addEventListnener('click', nameRandomizer)
+
+//Looks at the user's inputs
+//Function that loops through the number of trues from the input from the user
+//returns a name depending on the number of trues.
+function nameRandomizer(t){
+    let name = '';
+    if(t = 0){
+        return 'Method Man'
+    }else if(t = 1){
+        return 'RZA'
+    }else if(t = 2){
+        return 'Ol Dirty Bastard'
+    }else if(t = 3){
+        return 'Ghostface Killah'
+    }else if(t = 4){
+        return 'Raekwon'
+    }else{
+        return 'GZA'
+    }
+    document.querySelector('h1').textContent =
+    }
+
+    document.querySelectorAll()
+}
+
+//Values are 
